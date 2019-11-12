5293fcb5b6a12f48a79d3c384dfa5dd65a984051
Samantha Finley <srfinley@uchicago.edu>
2019-11-12 18:18:36 -0500

    real APIs, real tweets, real bloodshed

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 # app factory
 
@@ -7,13 +8,20 @@ def create_app():
     app = Flask(__name__)
 
     # add config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # tell database about app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
 
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset', users=[])
     return app
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -7,9 +7,21 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     """Twitter users to analyze"""
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
+    """Tweets from twitter"""
     id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    embedding = DB.Column(DB.PickleType, nullable=False)
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
